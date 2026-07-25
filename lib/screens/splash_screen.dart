d534d0e486a024d1956e0bfaf8e55cf08377d4b3
Nikesh1626 <nikesh.parihar2005@gmail.com>
2026-07-25 13:28:09 +0530

    style: simplify splash screen and increase logo size

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -9,39 +9,10 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _scaleAnimation;
-  late Animation<double> _fadeAnimation;
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
-    // Setup animation controller
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1500),
-    );
-
-    // Fade in from 0 to 1
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.0, 0.5, curve: Curves.easeIn),
-      ),
-    );
-
-    // Scale down slightly for a pop-in effect
-    _scaleAnimation = Tween<double>(begin: 0.8, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: const Interval(0.0, 0.8, curve: Curves.elasticOut),
-      ),
-    );
-
-    // Start the animation
-    _controller.forward();
 
     // Navigate to the main screen after a delay
     Timer(const Duration(milliseconds: 2500), () {
@@ -63,34 +34,17 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return FadeTransition(
-              opacity: _fadeAnimation,
-              child: ScaleTransition(
-                scale: _scaleAnimation,
-                child: Hero(
-                  tag: 'app_logo',
-                  child: Image.asset(
-                    'assets/logo.png',
-                    width: 180,
-                    height: 180,
-                  ),
-                ),
-              ),
-            );
-          },
+        child: Hero(
+          tag: 'app_logo',
+          child: Image.asset(
+            'assets/logo.png',
+            width: 240, // Increased size
+            height: 240, // Increased size
+          ),
         ),
       ),
     );
